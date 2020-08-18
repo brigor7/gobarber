48fc07dfdca09b6b4268f52c167bea091092bd67
=Bruno Igor <=brigor7@gmail.com>
2020-08-18 13:35:56 -0300

    Alter table

--- backend/src/database/migrations/1597691395867-CreateAppointments.ts
+++ backend/src/database/migrations/1597691395867-CreateAppointments.ts
@@ -16,12 +16,10 @@ export class CreateAppointments1597691395867 implements MigrationInterface {
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
         ],
       }),
